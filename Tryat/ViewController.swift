e0f39ef9b92cc943651f4c892706e2f2c9469551
Sahariar Kabir <sronok@gmail.com>
2017-11-01 00:52:27 +0600

    Initial Commit

---
+++ Tryat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tryat
+//
+//  Created by Sahariar Kabir on 11/1/17.
+//  Copyright © 2017 Sahariar Kabir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
