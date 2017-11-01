f6ccb9b037cbd107d7414850c29e15f6611487db
Sahariar Kabir <sronok@gmail.com>
2017-11-02 02:28:36 +0600

    endofaddandimplement_of_SWReveal

---
+++ Tryat/Controller/ChannelVC.swift
@@ -0,0 +1,22 @@
+//
+//  ChannelVC.swift
+//  Tryat
+//
+//  Created by Sahariar Kabir on 11/2/17.
+//  Copyright © 2017 Sahariar Kabir. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+		
+    }
+
+
+
+
+}
---
+++ Tryat/Controller/ChatVC.swift
@@ -0,0 +1,27 @@
+//
+//  ChatVC.swift
+//  Tryat
+//
+//  Created by Sahariar Kabir on 11/2/17.
+//  Copyright © 2017 Sahariar Kabir. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+	
+//	Outlets
+
+	@IBOutlet weak var menuBtn: UIButton!
+	
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+		self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+		self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+    
+
+
+}
