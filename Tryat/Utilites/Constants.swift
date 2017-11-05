d567a2c2076eb2add19d31be108f7b0246766ac8
Sahariar Kabir <sronok@gmail.com>
2017-11-05 07:53:34 +0600

    rgCaS

--- Tryat/Utilites/Constants.swift
+++ Tryat/Utilites/Constants.swift
@@ -7,9 +7,25 @@
 //
 
 import Foundation
-//segue
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+//url constent
+let base_Url = "http://localhost:3005/v1"
+let register_Url = "\(base_Url)/account/register"
+
+
+//segue
 let TO_LOGIN =  "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindtoChannel"
+//USER defaults
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+
+
+
+
 
